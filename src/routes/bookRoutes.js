7bff76b59b454f128c76051508e973e3208f5bc5
unknown <caestrada769@misena.edu.co>
2023-09-15 09:52:23 -0500

    nueva ruta book

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -7,6 +7,6 @@ router.get('/books', BookController.getAllBooks);
 router.get('/books/:id', BookController.getBookById);
 router.post('/books', BookController.createBook);
 router.put('/books/:id', BookController.updateBook);
-router.delete('books//:id', BookController.deleteBook);
+router.delete('/books/:id', BookController.deleteBook);
 
 module.exports = router;
